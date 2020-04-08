e4e68718963b59ff9e67e00297f48e86c363aa0c
Ashwin Gattani <gattani.ashwin@gmail.com>
2020-04-08 23:43:16 +0530

    consumer profile screen basic implementation done

--- src/components/consumer/profile/ConsumerProfile.js
+++ src/components/consumer/profile/ConsumerProfile.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, SafeAreaView} from 'react-native';
+import {Label, Text, Header, Body, Title} from 'native-base';
 
 export default class ConsumerProfile extends React.Component {
   render() {
-    return <View></View>;
+    return (
+      <SafeAreaView>
+        <Header>
+          <Body>
+            <Title>Profile</Title>
+          </Body>
+        </Header>
+        <View>
+          <Label>Name</Label>
+          <Label>Mobile</Label>
+          <Label>Address</Label>
+        </View>
+      </SafeAreaView>
+    );
   }
 }
