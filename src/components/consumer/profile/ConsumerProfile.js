9a29266b30df5842bcd0cebc9b462b0121105ea4
Ashwin Gattani <gattani.ashwin@gmail.com>
2020-04-10 00:45:23 +0530

    profile information shown based on user data from redux

--- src/components/consumer/profile/ConsumerProfile.js
+++ src/components/consumer/profile/ConsumerProfile.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import {View, SafeAreaView} from 'react-native';
 import {Label, Text, Header, Body, Title} from 'native-base';
+import {connect} from 'react-redux';
+import {getUser} from '../../../actions/UserActions';
 
-export default class ConsumerProfile extends React.Component {
+class ConsumerProfile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {},
+    };
+  }
+
+  componentDidMount() {
+    this.props.getUser();
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.user !== prevState.user) {
+      return {user: nextProps.user};
+    } else return null;
+  }
+
   render() {
     return (
       <SafeAreaView>
@@ -13,10 +32,31 @@ export default class ConsumerProfile extends React.Component {
         </Header>
         <View>
           <Label>Name</Label>
+          <Text> {this.state.user.name} </Text>
+        </View>
+        <View>
           <Label>Mobile</Label>
+          <Text> {this.state.user.mobileNumber} </Text>
+        </View>
+        <View>
           <Label>Address</Label>
+          <Text> {this.state.user.address} </Text>
         </View>
       </SafeAreaView>
     );
   }
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getUser: () => dispatch(getUser()),
+  };
+};
+
+const mapStateToProps = state => {
+  return {
+    user: state.user.user,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ConsumerProfile);
