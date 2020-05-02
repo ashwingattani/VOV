6bc0c0bc4406aaeb12c27fb21be236d9e98d974d
Ashwin Gattani <gattani.ashwin@gmail.com>
2020-05-02 16:12:20 +0530

    1. date format fixed in order details 2. order summary close button added

--- src/components/common/OrderItem.js
+++ src/components/common/OrderItem.js
@@ -5,19 +5,15 @@ import Modal from 'react-native-modal';
 import OrderSummary from './OrderSummary';
 
 function getDisplayFormatForDate(date) {
-  return date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear();
+  return (
+    date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear()
+  );
 }
 
 export default OrderItem = (props) => {
   let [render, setRender] = useState(false);
   return (
-    <View
-      style={styles.body}
-      onTouchEnd={() => {
-        if (render) {
-          setRender(false);
-        }
-      }}>
+    <View style={styles.body}>
       <Label>
         <Text> {getDisplayFormatForDate(new Date(props.item.date))} </Text>
       </Label>
@@ -32,6 +28,15 @@ export default OrderItem = (props) => {
           items={props.item.items}
           isCurrentOrder={props.isCurrentOrder}
         />
+        <Button
+          style={styles.modalActions}
+          onPress={() => {
+            if (render) {
+              setRender(false);
+            }
+          }}>
+          <Text>Close</Text>
+        </Button>
       </Modal>
     </View>
   );
@@ -45,8 +50,14 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   modal: {
-    flex: 1,
+    top: 40,
     opacity: 0.9,
-    bottom: 0,
+    marginBottom: 100,
+    justifyContent: 'space-evenly',
+  },
+  modalActions: {
+    top: 20,
+    alignSelf: 'center',
+    justifyContent: 'center',
   },
 });
